069f81a62cc376de2777d2f2e301ddcfcb995b64
michele byman <michelebyman@gmail.com>
2019-01-29 16:34:06 +0100

    functions changed not done yet

--- main.js
+++ main.js
@@ -11,23 +11,71 @@ for(let number of NUMBERS){
   $(".numbers").append("<button>" + number + "</button>");
 }
 for(let symbol of FUNCTION_BUTTON_2){
-  $(".functionButtons2").append("<button>" + symbol + "</button>");
+  $(".functionButtons2").append("<button class='func2'>" + symbol + "</button>");
 }
 
 // Create variables for buttons and display
 const BUTTONS = document.getElementsByTagName("button");
 const DISPLAY = $(".display");
 
+let numbers = [];
+let mathMethod;
+let clear = false;
+
 // Add event listeners to all buttons
-for (button of BUTTONS) {
-  button.addEventListener("click", function() {
-    // If the value in the display is 0..
-    if(DISPLAY[0].value == 0){
-      // ..Replace the value
-      DISPLAY[0].value = this.innerHTML;
-    }else{
-      // ..Append a value
-      DISPLAY[0].value += this.innerHTML;
-    }
-  });
+for (let button of BUTTONS) {
+
+  if (button.classList == "func2") {
+
+
+
+    button.addEventListener("click", function() {
+
+      if(button.innerHTML != mathMethod){ // ÄNDRA DENNA
+        if(numbers.length == 0){
+          numbers.push(DISPLAY[0].value);
+        }else{
+          numbers.push(DISPLAY[0].value);
+          console.log(numbers[0], numbers[1], mathMethod);
+          numbers.unshift(calc(Number(numbers[0]), Number(numbers[1]), mathMethod));
+          DISPLAY[0].value = numbers[0];
+          numbers.splice(1, 2);
+        }
+
+        if(button.innerHTML != "="){
+          mathMethod = button.innerHTML;
+        }
+
+        clear = true;
+        console.log(numbers);
+      }
+
+
+    });
+  }else{
+    button.addEventListener("click", function() {
+      // If the value in the display is 0..
+      if(DISPLAY[0].value == 0 || clear){
+        // ..Replace the value
+        DISPLAY[0].value = this.innerHTML;
+        clear = false;
+      }else{
+        // ..Append a value
+        DISPLAY[0].value += this.innerHTML;
+      }
+    });
+  }
+}
+
+function calc(a, b, method){
+  switch (method) {
+    case "+":
+    return a + b;
+    case "-":
+    return a - b;
+    case "*":
+    return a * b;
+    case "/":
+    return a / b;
+  }
 }
