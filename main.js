34095973cd2d840418091049d53130f4b4b598b6
Joakim <joakim.walden@gmail.com>
2019-01-29 11:03:49 +0100

    Created basic structure

---
+++ main.js
@@ -0,0 +1,16 @@
+const FUNCTION_BUTTON_1 = ["AC", "C", "onemore"];
+
+const FUNCTION_BUTTON_2 = ["/", "*", "-", "+", "="];
+
+const NUMBERS = [7,8,9,4,5,6,1,2,3,0,","];
+for (let i = 0; i < FUNCTION_BUTTON_1.length; i++) {
+  $(".functionButtons1").append("<button>" + FUNCTION_BUTTON_1[i] + "</button>");
+}
+
+for(number of NUMBERS){
+  $(".numbers").append("<button>" + number + "</button>");
+}
+
+for(symbol of FUNCTION_BUTTON_2){
+  $(".functionButtons2").append("<button>" + symbol + "</button>");
+}
